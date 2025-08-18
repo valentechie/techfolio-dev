fb41626350c53905893b9bf90ca1004c133aa18a
arminta <vg.arminta.2024@alumnos.urjc.es>
2025-08-18 16:33:29 +0200

    Update files

--- script.js
+++
@@ -1,5 +0,0 @@
-// Modo oscuro simple (cambia fondo y texto)
-document.getElementById('toggle-dark').addEventListener('click', function(e) {
-  e.preventDefault();
-  document.body.classList.toggle('dark-mode');
-});
